7e047563dfe038d95607211d9b1fd4b19e4d6f9c
MatheusRdk <matheus_rudek@hotmail.com>
2023-04-17 12:16:49 -0300

    Exceptions pt.1

---
+++ exception/error/teste/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package exception.error.teste;
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+
+    public static void recursividade(){   //Erro é um processo que ocorre na JVM, e dificilmente vc vai parar em tempo de execução (tem que encerrar o processo). Erro é subclasse de throwable e nao e uma 
+        //                                    exception. 
+        recursividade();                 //Nesse caso, o codigo vai gerar um stackoverflow error, que acontece por falta de memoria.
+    }
+}
