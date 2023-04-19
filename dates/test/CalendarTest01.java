f5b331cb6a706030bbd8529184b0aae3cffc14a3
MatheusRdk <matheus_rudek@hotmail.com>
2023-04-19 11:55:06 -0300

    strings dates

---
+++ dates/test/CalendarTest01.java
@@ -0,0 +1,32 @@
+package dates.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        //Calendar c = new Calendar(); Nao da pra fazer isso pois esse calendar é abstrato.
+        Calendar c = Calendar.getInstance(); //Pega uma instancia do calendar e coloca na variavel de referencia c.
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {  //Em alguns países o domingo ou é primeiro dia ou é ultimo dia da semana. Pra saber como está definido aqui faz isso.
+            System.out.println("Domingao do faustoao é o primeiro dia da semana");
+        }
+
+        Date date = c.getTime(); // Vc pega o dia de hj dessa forma, pois o c sozinho tem um formato complicado.
+        System.out.println(date);
+
+        System.out.println("-------------------");
+
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));  //Usando as constantes vc pode pegar coisas úteis.
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+
+        System.out.println("-------------------");
+
+        c.add(Calendar.DAY_OF_MONTH, 2); //Adicionando dois dias na data de hj.
+        c.roll(Calendar.HOUR, 3); //Com o roll vc adiciona tempo sem virar o dia, quando passa de 24 hrs ele volta pro 0 no mesmo dia.
+
+        Date date2 = c.getTime();
+        System.out.println(date2);
+    }
+}
---
+++ dates/test/DateTest01.java
@@ -0,0 +1,12 @@
+package dates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1681914346534L); //Quase nao é usado esse date, pois é dos sistemas legados. Mas é importante caso mexa com um sistema legado, abaixo do java 7.
+        //O date em ms dessa classe Date representa todas as datas em ms desde 1970.
+        System.out.println(date); //O tempo no date é em ms, e o sout converte pra uma forma legível com o to.String(). Para printar com o numero em ms mesmo, usa date.getTime().
+        System.out.println(date.getTime()); //Desse jeito vc pega o dia de hj em ms.
+    }
+}
