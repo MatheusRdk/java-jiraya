48ea56df5bd2c259f676ea22a8d109a246ab8307
MatheusRdk <matheus_rudek@hotmail.com>
2023-04-26 15:10:53 -0300

    regex

--- src/regex/test/PatternMatcherTest03.java
+++ src/regex/test/PatternMatcherTest03.java
@@ -3,7 +3,7 @@ package regex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest03 {
     public static void main(String[] args) {
         // \d = Todos os dígitos
         // \D = Tudo o que nao for dígito.
@@ -11,10 +11,13 @@ public class PatternMatcherTest02 {
         // \S = Tudo menos espaço em branco
         // \w = a-zA-z, dígitos, _
         // \W = tudo que nao for incluso no \w
+        // []
 
-        String regex = "\\d";
-        String texto = "abaabababa";
-        String texto2 = "asjkd123123nklj23890";
+
+        //String regex = "[a-zA-C]";
+        String regex = "0[xX][a-fA-F\\d]";
+        //String texto = "abaabababa";
+        String texto2 = "0x 12 0x14D2 0X 0xBBA 0X109 0x1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("texto:  " + texto2);
@@ -24,5 +27,8 @@ public class PatternMatcherTest02 {
         while (matcher.find()){
             System.out.print(matcher.start() + " " + matcher.group() + "\n");  //O group mostra o que ele achou e o start a posição.
         }
+
+        int numeroHex = 0X109;  //Para o java reconhecer um numero hexadecimal vc coloca 0x ou 0X na frente. O hex so pode ter letra até F
+        System.out.println(numeroHex);
     }
 }
--- src/regex/test/PatternMatcherTest04.java
+++ src/regex/test/PatternMatcherTest04.java
@@ -3,7 +3,7 @@ package regex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest04 {
     public static void main(String[] args) {
         // \d = Todos os dígitos
         // \D = Tudo o que nao for dígito.
@@ -11,13 +11,21 @@ public class PatternMatcherTest02 {
         // \S = Tudo menos espaço em branco
         // \w = a-zA-z, dígitos, _
         // \W = tudo que nao for incluso no \w
+        // [] = procura qualquer coisa que esteja dentro dos colchetes
+        // ? = Zero ou 1 ocorrência
+        // * = Zero ou mais ocorrências
+        // + = Uma ou mais ocorrências
+        // {n, m} = De n a m ocorrências
+        // () = agrupamento
+        // | = ou
+        // $ = fim da linha
+        // . = tudo
 
-        String regex = "\\d";
-        String texto = "abaabababa";
-        String texto2 = "asjkd123123nklj23890";
+        String regex = "0[xX]([a-fA-F\\d])+(\\s|$)";  //no fim significa que as letras e numeros sao seguidas ou de um espaço em branco ou fim de linha
+        String texto = "0x 12 0x14D2 0X 0xBBA 0X10G 0x1";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto2);
-        System.out.println("texto:  " + texto2);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  " + texto);
         System.out.println("indice: 0123456789");
         System.out.println("regex:  " + regex);
         System.out.println("Posições encontradas");
