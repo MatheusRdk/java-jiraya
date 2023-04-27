ceb6b6106928c9b5604f6d1f61ea64839416f9f2
MatheusRdk <matheus_rudek@hotmail.com>
2023-04-27 20:41:23 -0300

    file attributes

---
+++ src/nio/test/DirectoryStreamTest01.java
@@ -0,0 +1,20 @@
+package nio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest01 {   //Uma forma de listar todos os arquivos de um diretorio
+    public static void main(String[] args) {
+        Path dir = Paths.get(".");
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for (Path path: stream){
+                System.out.println(path.getFileName());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
