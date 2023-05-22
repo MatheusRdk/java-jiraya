6b2562cf5cd23c7e622262731916b3f309694085
MatheusRdk <matheus_rudek@hotmail.com>
2023-05-22 11:50:24 -0300

    singleton

---
+++ src/padroesdeprojeto/test/AircraftSingletonEagerTest01.java
@@ -0,0 +1,16 @@
+package padroesdeprojeto.test;
+
+import padroesdeprojeto.dominio.Aircraft;
+import padroesdeprojeto.dominio.AircraftSingletonEager;
+
+public class AircraftSingletonEagerTest01 { //Padrao Singleton Eager
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A"); //Agora vai dar false, pq nao importa onde trabalhe com AircraftSingletonEager sempre sera com o mesmo objeto, mesmo espaço em memória.
+    }
+
+    private static void bookSeat(String seat){
+        AircraftSingletonEager aircraftSingletonEager = AircraftSingletonEager.getINSTANCE(); //Vc nao pode mais dar new, o construtor dele é privado.
+        System.out.println(aircraftSingletonEager.bookSeat(seat));
+    }
+}
