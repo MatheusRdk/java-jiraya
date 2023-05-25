c80fd27fed0fee99436c3ff080dfce1c42145b7d
MatheusRdk <matheus_rudek@hotmail.com>
2023-05-25 12:17:02 -0300

    service

--- src/academy/jdbc/service/ProducerService.java
+++ src/academy/jdbc/service/ProducerService.java
@@ -5,7 +5,7 @@ import academy.jdbc.repository.ProducerRepository;
 
 public class ProducerService { //Essa é a camada de serviço, ela serve pra ser mais uma camada entre o que o cliente esta consumindo e o a classe que ele usa. Por ex.: a classe repository
     //vai criar os métodos de inserção e delete no banco de dados. Mas vc nao usa ela diratemente no na classe main principal (que consome o serviço), vc cria essa de serviço que puxa
-    //todos os métodos do repository e ai usa através dela lá na classe 'consumidora' (que tem o psvm).
+    //todos os métodos do repository e ai usa através dela lá na classe 'consumidora' (que tem o public static void main).
     //Aqui vc faz a regra de negócio também, por ex. os requisitos pra usar os métodos do repository, como o delete ali, vc checa aqui se o id é valido pra ser usado ou nao.
 
     public static void save(Producer producer){
